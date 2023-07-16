7b345fca44ff4fae0b5e02c900675027a50e67af
itheima <itheima@itcast.cn>
2023-07-16 17:45:46 +0800

    项目初始化-调整初始目录结构

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <!-- 路由的出口 -->
+    <h1>黑马头条</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name : "app"
+  }
+</script>
+
+<style lang="less">
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+Vue.use(VueRouter)
+
+/* 自己的路由表 */
+const routes = [
+  
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
